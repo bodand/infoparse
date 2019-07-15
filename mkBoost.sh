84055fdae31a8efab243fa50b1c1efd512892205
bodand <bodand@pm.me>
2019-07-15 14:43:16 +0200

    typo
    Signed-off-by: bodand <bodand@pm.me>

--- mkBoost.sh
+++ mkBoost.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p -- /usr/local/include
-curl -Ls https://isdevnet.com/boost/include | tar xJC /usr/local/include --strip-componets=1
+curl -Ls https://isdevnet.com/boost/include | tar xJC /usr/local/include --strip-components=1
 
 mkdir -p -- /usr/local/lib
 curl -Ls https://isdevnet.com/boost/lib | tar xJC /usr/local/lib
