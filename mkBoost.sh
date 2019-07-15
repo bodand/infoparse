8c35b87b62b8bd95ef5033a09c0329b40a1b9ba2
bodand <bodand@pm.me>
2019-07-15 17:12:20 +0200

    Let's see what's where?
    Signed-off-by: bodand <bodand@pm.me>

--- mkBoost.sh
+++ mkBoost.sh
@@ -4,7 +4,9 @@ echo [Boosting Time]
 
 mkdir -p -- /usr/local/include
 curl -Ls https://isdevnet.com/boost/include | tar xJC /usr/local/include
+ls /usr/local/include
 
 mkdir -p -- /usr/local/lib
 curl -Ls https://isdevnet.com/boost/lib | tar xJC /usr/local/lib
+ls /usr/local/lib
 
