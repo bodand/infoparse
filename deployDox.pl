437b9bd8bdb393ca96e8d46b6a9d99ab0bca1495
bodand <bodand@pm.me>
2019-07-16 15:07:17 +0200

    Deployment ;_;
    Signed-off-by: bodand <bodand@pm.me>

---
+++ deployDox.pl
@@ -0,0 +1,43 @@
+use v5.10;
+use warnings;
+
+use Digest::MD5 qw/md5_hex md5_base64/;
+use MIME::Base64;
+use JSON::MaybeXS qw/encode_json to_json/;
+
+use Data::Dumper;
+
+my $deployer = "https://deployer.isdevnet.com";
+
+sub read_base64 {
+    open my $file, "<:raw", shift
+        or die "Unusable file supplied";
+    local $/ = undef;
+    encode_base64(<$file>)
+}
+
+sub read_md5 {
+    open my $file, "<:raw", shift
+        or die "Unusable file supplied";
+    local $/ = undef;
+    md5_hex(<$file>)
+}
+
+my $data = {
+    pwd      => $ENV{DEPLOYER_PASSWORD} // die "Undefined environment variable of password",
+    usr      => $ENV{DEPLOYER_USERNAME} // die "Undefined environment variable of username",
+    compflag => 'J',
+    name     => 'docs.tar.xz',
+    file     => read_base64('docs.tar.xz'),
+    md5      => read_md5('docs.tar.xz'),
+};
+
+open my $curlFile, '>', 'deploy.curl'
+    or die "Can't open curl configuration file.";
+print $curlFile "-d " . to_json($data);
+close $curlFile;
+
+system 'curl', $deployer, '-K', 'deploy.curl';
+
+unlink "deploy.curl";
+
